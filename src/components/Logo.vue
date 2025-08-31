a5139ae338462fa3cdee4333ba3288e2b23e9e58
MONCHÂTRE <tmonchatre@gmail.com>
2025-08-31 13:37:53 +0200

    Ajout badge

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,17 +1,19 @@
-<!-- <script setup>
+<script setup>
 import { ref } from 'vue'
 
+const nbClick = ref(0)
 
-<script> -->
+</script>
 
 <template>
  <div class="header shadow-lg p-3 mb-5 rounded">
     <div class="container logo">
       <div class="row justify-content-center">
-        <div class="col-4 logo-img">
+        <div @click="nbClick++" class="col-4 logo-img">
           <img src="../../img/oreo1.svg" alt="Oréo" />
+          <span class="badge text-bg-secondary">{{nbClick}}</span>
         </div>
-      </div>
     </div>
-  </div>
+    </div>
+</div>
 </template>
